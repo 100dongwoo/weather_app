f1ab08f616393ec17f4823923a4d8d6771ab3707
BAEK DONGWOO <qorehddn123@naver.com>
2021-06-17 17:06:45 +0900

    flex-box 변경

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>asd</Text>
+            <View style={styles.yellowView}/>
+            <View style={styles.blueView}/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    yellowView: {
+        flex: 1,
+        backgroundColor: "yellow"
+    },
+    blueView: {
+        flex: 1,
+        backgroundColor: "blue"
+    }
 });
